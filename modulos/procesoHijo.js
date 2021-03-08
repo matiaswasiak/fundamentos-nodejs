ffe088a831872ef5b85eba9cc1caf2bc2f4598e8
matiaswasiak <matiaswasiak@gmail.com>
2021-03-08 18:26:41 -0300

    Procesos hijo

--- modulos/procesoHijo.js
+++ modulos/procesoHijo.js
@@ -1,1 +1,23 @@
-const { exec } = require("child_process");
+const { exec, spawn } = require("child_process");
+
+// exec("node modulos/console.js", (err, stdout, sterr) => {
+//   if (err) {
+//     console.error(err);
+//     return false;
+//   }
+
+//   console.log(stdout);
+// });
+
+let proceso = spawn("ls", ["-la"]);
+
+console.log(proceso.pid);
+console.log(proceso.connected);
+
+proceso.stdout.on("data", (dato) => {
+  console.log(dato.toString());
+});
+
+proceso.on("exit", () => {
+  console.log("el proceso terminó");
+});
