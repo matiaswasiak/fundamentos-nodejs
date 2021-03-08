059969d8d452e4c5d2bb8c0fbd172242ea65f3b7
matiaswasiak <matiaswasiak@gmail.com>
2021-03-08 17:19:55 -0300

    Async/await

---
+++ async/asyncAwait.js
@@ -0,0 +1,33 @@
+async function hola(nombre) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      console.log("Hola, " + nombre);
+      resolve(nombre);
+    }, 1500);
+  });
+}
+
+function hablar(nombre) {
+  return new Promise((resolve, reject) => {
+    setTimeout(function () {
+      console.log("Bla bla bla bla...");
+      // resolve(nombre);
+      resolve("Hay un error");
+    }, 1000);
+  });
+}
+
+function adios(nombre) {
+  return new Promise((resolve, reject) => {
+    setTimeout(function () {
+      console.log("Adios", nombre);
+      resolve();
+    }, 1000);
+  });
+}
+
+async function main() {
+  await hola("Matias");
+}
+
+main();
