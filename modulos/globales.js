103e2c26f4a2201337686ec0452cd06202b07d41
matiaswasiak <matiaswasiak@gmail.com>
2021-03-08 17:29:42 -0300

    Globals

---
+++ modulos/globales.js
@@ -0,0 +1,12 @@
+// let i = 0;
+// let intervalo = setInterval(() => {
+//   console.log("Hola");
+//   if (i === 3) {
+//     clearInterval(intervalo);
+//   }
+//   i++;
+// }, 1000);
+
+setImmediate(() => {
+  console.log("Hola");
+});
