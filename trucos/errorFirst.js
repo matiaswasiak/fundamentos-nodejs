5760ab4df5dbb139208e73612143a49f0effcf99
matiaswasiak <matiaswasiak@gmail.com>
2021-03-15 22:01:16 -0300

    Error First Callbacks

---
+++ trucos/errorFirst.js
@@ -0,0 +1,21 @@
+function asincrona(callback) {
+  setTimeout(() => {
+    try {
+      let a = 3 + z;
+      callback(null, a);
+    } catch (e) {
+      callback(e);
+    }
+  }, 1000);
+}
+
+asincrona((err, dato) => {
+  if (err) {
+    console.error("Tenemos un error");
+    console.error(err);
+    // throw err;
+    return false;
+  }
+
+  console.log("todo ha ido bien, mi data es", dato);
+});
